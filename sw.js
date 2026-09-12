dd11e497a4567e21230e932bd9065c679a1b4f38
deepanshuchauhan716 <deepanshu9873250@gmail.com>
2026-09-12 11:27:37 +0530

    add pwa feature

---
+++ sw.js
@@ -0,0 +1,14 @@
+const CACHE_NAME = 'apibreak-v1';
+const urlsToCache = ['./', './dashboard.html', '/landingpage.html'];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => response || fetch(event.request))
+    );
+});
